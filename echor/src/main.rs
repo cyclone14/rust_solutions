3f219140d527ae3b62eb24e817c07c4f2e56b3fa
cyclone14 <jprice1947@comcast.net>
2024-11-24 23:18:04 -0500

    Save progress

---
+++ echor/src/main.rs
@@ -0,0 +1,23 @@
+use clap::{Arg, ArgAction, Command};
+fn main() {
+    let matches = Command::new("echor")
+    .version("0.1.0")
+    .author("Jonn Price <jprice1947@comcast.net")
+    .about("Rust version of 'echo'")
+    .arg(
+        Arg::new("text")
+        .value_name("TEXT")
+        .help("Input text")
+        .required(true)
+        .num_args(1..),
+    )
+    .arg(
+        Arg::new("omit_newline")
+        .short('n')
+        .action(ArgAction::SetTrue)
+        .help("Do not print newline"),
+    )
+    .get_matches();
+
+    println!("{:#?}", matches);
+}
